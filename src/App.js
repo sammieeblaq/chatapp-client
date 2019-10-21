96cbc2083bc7c286e07ba9c9479183da7f94a5e5
Samuel Airehrour <odianoseairehrour@gmail.com>
2019-10-21 23:18:52 +0100

    Added the Chat.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+// Import of Components
+import Join from "./components/Join/Join";
+import Chat from "./components/Chat/Chat";
+
 
 const App = () => (
-    <Router>
+    <Router>git git 
         <Route path="/"  exact component={Join} />
-        <Route path="/chat" component={Join} />
+        <Route path="/chat" component={Chat} />
     </Router>
 )
 
---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const chat = () => (
+    <div>
+        <h1>Chat</h1>
+    </div>
+)
+
+export default chat;
