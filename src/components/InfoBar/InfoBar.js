91a3c7bd771d8d358f985d444d02ac42ba992b66
Samuel Airehrour <odianoseairehrour@gmail.com>
2019-11-03 09:34:53 +0100

    Fixed the bug in the messages component

---
+++ src/components/InfoBar/InfoBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import onlineIcon from "../../icons/onlineIcon.png";
+import closeIcon from "../../icons/closeIcon.png";
+import "./InfoBar.css";
+
+const InfoBar = ({ room }) => (
+    <div className="infoBar">
+        <div className="leftInnerContainer">
+            <img className="onlineIcon" src={onlineIcon} alt="online Icon" />
+            <h3>{ room }</h3>
+        </div>
+        <div className="rightInnerContainer">
+            <a href="/"><img className="closeIcon" src={closeIcon} alt="Close Icon" /></a>
+        </div>
+    </div>
+)
+
+export default InfoBar;
